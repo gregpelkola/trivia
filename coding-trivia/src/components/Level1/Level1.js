75d2d6478cb5f03d8d773b276f5d1db1ac692f2c
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-04-07 18:21:14 -0500

    created grid for word search in level!

--- coding-trivia/src/components/Level1/Level1.js
+++ coding-trivia/src/components/Level1/Level1.js
@@ -4,10 +4,23 @@ import './Level1.css';
 function Level1({ progressToNextLevel }) {
     const [selectedWord, setSelectedWord] = useState('');
     const [selectedPositions, setSelectedPositions] = useState([]);
-    const wordsToFind = ["HTML", "CSS", "JAVASCRIPT", "REACT", "NODE", "EXPRESS", "MONGODB", "MERN", "FULLSTACK"];
+    const wordsToFind = ["HTML", "CSS", "JAVASCRIPT", "REACT", "NODE", "EXPRESS", "MONGODB", "MERN", "STRING", "ARRAY", "FUNCTION", "BOOLEAN"];
     const [foundWords, setFoundWords] = useState([]);
 }
 
 const grid = {
-    
-}
+    ['J', 'S', 'C', 'S', 'C', 'R', 'E', 'A', 'C', 'T'],
+    ['N', 'O', 'D', 'E', 'U', 'X', 'I', 'R', 'E', 'S'],
+    ['Z', 'M', 'E', 'R', 'N', 'Y', 'O', 'K', 'C', 'M'],
+    ['O', 'L', 'L', 'M', 'O', 'N', 'G', 'O', 'D', 'B'],
+    ['D', 'B', 'A', 'S', 'S', 'T', 'R', 'I', 'N', 'G'],
+    ['F', 'U', 'N', 'C', 'T', 'I', 'O', 'N', 'B', 'O'],
+    ['O', 'O', 'A', 'R', 'R', 'A', 'Y', 'G', 'D', 'B'],
+    ['H', 'T', 'M', 'L', 'B', 'T', 'R', 'W', 'N', 'U'],
+    ['J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T'],
+    ['Z', 'K', 'B', 'T', 'C', 'S', 'S', 'G', 'B', 'O'],
+    ['E', 'X', 'P', 'R', 'E', 'S', 'S', 'O', 'D', 'E'],
+    ['P', 'E', 'R', 'B', 'O', 'O', 'L', 'E', 'A', 'N'],
+};
+
+const [letterColors, setLetterColors] = useState(Array(grid.length).fill().map(()));
